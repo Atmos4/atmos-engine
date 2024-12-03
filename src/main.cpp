38bf29a877e12fba6d8fd960369cb41fe4b1264c
Atmos4 <atmos4.dev@gmail.com>
2024-12-03 16:44:38 +0100

    throttle fps

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@
 const int TICKS_PER_SECOND = 25;
 const int DELAY = 1000 / TICKS_PER_SECOND;
 const int MAX_FRAMESKIP = 5;
-
+const float TARGET_FPS = 150;
+const float frameDelay = 1000.0f / TARGET_FPS;
 
 Engine engine;
 Game game;
@@ -46,6 +47,19 @@ int main(int argc, char** argv) {
 	#ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(mainloop, 0, 1);
     #else
-    while (1) { mainloop(); }
+	Uint64 frameStart = SDL_GetPerformanceCounter();
+	Uint64 frameEnd;
+	double deltaTime;
+    while (1) { 
+		frameStart = SDL_GetPerformanceCounter();
+		mainloop();
+		frameEnd = SDL_GetPerformanceCounter();
+    	deltaTime = (frameEnd - frameStart) * 1000.0 / SDL_GetPerformanceFrequency();
+
+		// Delay to throttle frame rate
+		if (deltaTime < frameDelay) {
+			SDL_Delay(frameDelay - deltaTime);
+		}
+	}
     #endif
 }
